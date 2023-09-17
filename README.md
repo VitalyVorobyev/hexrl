47e9539af467059cdb55bcf9f71a1cdd7437ceb8
Vitaly Vorobyev <vit.vorobiev@gmail.com>
2023-09-17 17:37:52 +0200

    random games in python

--- README.md
+++ README.md
@@ -2,4 +2,37 @@
 
 A pet project of a reinforcement agent for the [Hex game](https://en.wikipedia.org/wiki/Hex_(board_game)). A browser GUI is implemented with [React](https://github.com/facebook/react), game driver is in python. Communication between the driver and the GUI is done via JsonRPC.
 
-The project is at initial stage. Nothin to show yet.
+The project is at initial stage. Only random games with ASCII board implemented so far
+
+```bash
+  _____
+ /     \
+/   o   \_____
+\       /     \
+ \_____/   x   \_____
+ /     \       /     \
+/   .   \_____/   o   \_____
+\       /     \       /     \
+ \_____/   x   \_____/   .   \_____
+ /     \       /     \       /     \
+/   o   \_____/   o   \_____/   x   \
+\       /     \       /     \       /
+ \_____/   x   \_____/   x   \_____/
+ /     \       /     \       /     \
+/   o   \_____/   o   \_____/   o   \
+\       /     \       /     \       /
+ \_____/   o   \_____/   o   \_____/
+ /     \       /     \       /     \
+/   x   \_____/   o   \_____/   x   \
+\       /     \       /     \       /
+ \_____/   x   \_____/   x   \_____/
+       \       /     \       /     \
+        \_____/   x   \_____/   .   \
+              \       /     \       /
+               \_____/   x   \_____/
+                     \       /     \
+                      \_____/   .   \
+                            \       /
+                             \_____/
+Blue wins (x)
+```
--- py/hexgame.py
+++ py/hexgame.py
@@ -7,26 +7,133 @@ class HexDriver:
     """ Hex game mechanics """
     def __init__(self, size:int=11):
         """ Ctor """
+        self.game_over = False
         self.moves = 0
         self.blue = 1
         self.red = -1
         self.position = np.zeros((size, size), dtype=int)
+        self.rng = np.random.default_rng()
+
+    def make_random_move(self):
+        """ """
+        if self.game_over:
+            return None
+        if self.position.size > self.moves + 1:
+            move = self.rng.integers(self.position.size - self.moves - 1, size=1).item()
+        else:
+            move = 0  # last move
+        rows, cols = np.where(self.position == 0)
+        hsize = self.position.shape[0] // 2
+        q = cols[move] - hsize
+        r = rows[move] - hsize
+        # print(self.red_moves(), end=' ')
+        self.make_move(q, r)
+        # print(f'{q:+d} {r:+d} {rows.size:2d} {self.moves:2d} {self.game_over}')
+        return q, r
+
+    def make_move(self, q:int, r:int) -> bool:
+        """ """
+        if self.game_over:
+            print('No moves: game over')
+            return False
+        if not self.is_in_board(q, r) or self.cell_color(q, r) != 0:
+            print(f'Bad move: {q:+d} {r:+d} {self.is_in_board(q, r)} {self.cell_color(q, r)}')
+            return False
+        hsize = self.position.shape[0] // 2
+        col, row = q + hsize, r + hsize
+        self.position[row, col] = self.red if self.red_moves() else self.blue
+
+        self.moves += 1
+        self.game_over = self.is_game_over()
+
+        return True
     
-    def __str__(self) -> str:
-        smap = {-1: 'x', 0: '.', 1: 'o'}
+    def is_game_over(self):
+        if self.game_over:
+            return self.game_over
+        return (self.blue_wins() if self.red_moves() else self.red_wins())
+
+    def red_moves(self) -> bool:
+        """ True if red makes the next move """
+        return self.moves % 2
+
+    def is_in_board(self, q:int|hexpex.Axial, r:int|None=None) -> bool:
         size = self.position.shape[0]
-        total_lines = size * 5 + 2 * (size - 1)
-        lines = ['' for _ in range(total_lines)]
+        hsize = self.position.shape[0] // 2
+        col, row = (q.q, q.r) if r is None else (q, r)
+        col, row = col + hsize, row + hsize
+        return col >= 0 and col < size and row >= 0 and row < size
+
+    def cell_color(self, q:int|hexpex.Axial, r:int|None=None) -> int:
+        hsize = self.position.shape[0] // 2
+        col, row = (q.q, q.r) if r is None else (q, r)
+        return self.position[row + hsize, col + hsize]
+    
+    def blue_wins(self) -> bool:
+        """ True if a winning position for blue """
+        # print('blue_wins', end=' ')
+        hsize = self.position.shape[0] // 2
+        sources = [hexpex.Axial(-hsize, r) for r in range(-hsize, hsize + 1) if self.cell_color(-hsize, r) == self.blue]
+        targets = [hexpex.Axial( hsize, r) for r in range(-hsize, hsize + 1) if self.cell_color( hsize, r) == self.blue]
+        return self.dfs(sources, targets)
+    
+    def red_wins(self) -> bool:
+        """ """
+        hsize = self.position.shape[0] // 2
+        sources = [hexpex.Axial(q, -hsize) for q in range(-hsize, hsize + 1) if self.cell_color(q, -hsize) == self.red]
+        targets = [hexpex.Axial(q,  hsize) for q in range(-hsize, hsize + 1) if self.cell_color(q,  hsize) == self.red]
+        return self.dfs(sources, targets)
+    
+    def same_color(self, cell1:hexpex.Axial, cell2:hexpex.Axial) -> bool:
+        return self.cell_color(cell1) == self.cell_color(cell2)
+
+    def dfs(self, sources:list[hexpex.Axial], targets:set[hexpex.Axial]) -> bool:
+        """ True if any of targets is reachable from any of sources """
+        if len(targets) == 0 or len(sources) == 0:
+            return False
+        visited = set()
+        while len(sources) != 0:
+            curr = sources.pop()
+            if curr in targets:
+                return True
+            visited.add(curr)
+
+            for neib in curr.ring(1):
+                if neib not in visited and\
+                        self.is_in_board(neib) and\
+                        self.same_color(curr, neib):
+                    sources.append(neib)
+        return False
+
+    def __str__(self) -> str:
+        smap = {-1: 'o', 0: '.', 1: 'x'}
+        size = self.position.shape[0]
+        total_lines = 1 + size * 4 + 2 * (size - 1)
+        lines = ['       ' for _ in range(total_lines)]
 
         lines[0] = '  _____'
         for row in range(size):
-            irow = 4 * row
-            lines[irow + 1] = ' /     \ '
-            lines[irow + 2] = '/       \\'
-            lines[irow + 3] = '\       /'
-            lines[irow + 4] = ' \_____/'
+            color = smap[self.position[row, 0]]
+            irow = 1 + 4 * row
+            lines[irow + 0] = ' /     \ '
+            lines[irow + 1] = f'/   {color}   \\'
+            lines[irow + 2] = '\       /'
+            lines[irow + 3] = ' \_____/ '
 
-        
+        for col in range(1, size):
+            lines[2 * col] += '_____'
+            lines[2 * col + 4 * size - 1] += '\ '
+            lines[2 * col + 4 * size] += ' \\'
+            for idx in range(2 * col + 4 * size + 1, total_lines):
+                lines[idx] += '       '
+            for row in range(size):
+                color = smap[self.position[row, col]]
+                irow = 1 + 2 * col + 4 * row
+                lines[irow + 0] += '     \ '
+                lines[irow + 1] += f'  {color}   \\'
+                lines[irow + 2] += '      /'
+                lines[irow + 3] += '_____/ '
+
         return '\n'.join(lines)
 
 #    _____
@@ -46,73 +153,17 @@ class HexDriver:
 #          \_____/   x   \
 #                \       /
 #                 \_____/
-  
-    def make_move(self, q:int, r:int) -> bool:
-        """ """
-        if not self.is_in_board(q, r) or self.cell_color(q, r) != 0:
-            return False
-        hsize = self.position.shape[0] // 2
-        col, row = q + hsize, r + hsize
-        self.position[row, col] = self.red if self.moves % 2 else self.blue
-        return True
-
-    def red_moves(self) -> bool:
-        """ True if red makes the next move """
-        return self.moves % 2
-
-    def is_in_board(self, q:int|hexpex.Cube, r:int|None) -> bool:
-        hsize = self.position.shape[0] // 2
-        col, row = q.q, q.r if r is None else q, r
-        col, row = col + hsize, row + hsize
-        return col > 0 and col < size and row > 0 and row < size
-
-    def cell_color(self, q:int|hexpex.Cube, r:int|None) -> int:
-        hsize = self.position.shape[0] // 2
-        col, row = q.q, q.r if r is None else q, r
-        return self.position[row + hsize, col + hsize]
-    
-    def blue_wins(self) -> bool:
-        """ True if a winning position for blue """
-        hsize = self.position.shape[0] // 2
-        sources = [hexpex.Cube(-hsize, r, hsize - r) for r in range(-hsize, hsize + 2)
-                   if self.cell_color(-hsize, r) == self.blue]
-        targets = [hexpex.Cube(hsize + 1, r, -hsize - r - 1) for r in range(-hsize, hsize + 2)
-                   if self.cell_color(hsize + 1, r) == self.blue]
-        return self.dfs(sources, targets)
-    
-    def red_wins(self) -> bool:
-        """ """
-        hsize = self.position.shape[0] // 2
-        sources = [hexpex.Cube(q, -hsize, hsize - q) for q in range(-hsize, hsize + 2)
-                   if self.cell_color(q, -hsize) == self.blue]
-        targets = [hexpex.Cube(q, hsize + 1, -hsize - q - 1) for q in range(-hsize, hsize + 2)
-                   if self.cell_color(q, hsize + 1) == self.blue]
-        return self.dfs(sources, targets)
-    
-    def same_color(self, cell1:hexpex.Cube, cell2:hexpex.Cube) -> bool:
-        return self.cell_color(cell1) == self.cell_color(cell2)
-
-    def dfs(self, sources:list[hexpex.Cube], targets:set[hexpex.Cube]) -> bool:
-        """ True if any of targets is reachable from any of sources """
-        if len(targets) == 0 or len(sources) == 0:
-            return False
-        visited = set()
-        while len(sources) != 0:
-            curr = sources.pop()
-            if curr in targets:
-                return True
-            visited.add(curr)
-
-            for neib in curr.ring(1):
-                if neib not in visited and\
-                        self.is_in_board(neib) and\
-                        self.same_color(curr, neib):
-                    sources.append(neib)
-        return False
-
 
 if __name__ == '__main__':
     """ """
-    size = 3
+    size = 5
     board = HexDriver(size=size)
+    # while not board.game_over:
+    for _ in range(size**2):
+        board.make_random_move()
     print(board)
+    assert board.game_over
+    if board.red_moves():
+        print('Blue wins (x)')
+    else:
+        print('Red wins (o)')
